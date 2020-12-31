9b0e07c9722b2659558908c34682d1389be5a931
kaushik asokan <kaushikam@gmail.com>
2020-12-31 15:12:36 +0530

    Placed all configurations to JS

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # OUath 2.0 Authorization Code Flow - Vanilla JS Client
 
 This code collect authorization code from the `Keycloak` server and then using that _authorization code_ gets the _access token_ and _refersh token_. Using the _access token_ accesses the resource server which will respond with a list of heroes (_As in angular.io heroes example_).
+
+Create `app-config.js` with content as in `app-config.js.example` and replace values according to your configuration.
+
+I am using [https-localhost](https://github.com/daquinoaldo/https-localhost) as the `https` server. When you are using the `PKCE` branch, you need an _https_ server for the _crypto_ code to work. In my opinion _https-localhost_ is a pretty good tool for that.
--- auth-code.js
+++ auth-code.js
@@ -9,9 +9,9 @@ var randomString = function(length) {
 
 var getAuthorizationCode = function() {
     var state = randomString(20);
-    var authParams = 'client_id=spa-heroes&redirect_uri=http://localhost:8000&scope=heroes openid&state=' + state + '&response_type=code';
-    var authUrl = 'http://localhost:9000/auth/realms/heroes/protocol/openid-connect/auth' + '?' + authParams;
-    window.location = authUrl;
+    authCodeConfig.state = state;
+    var authParams = createParams(authCodeConfig, true);
+    window.location = urlConfig.auth_url + authParams;
 };
 
 window.onload = function() {
@@ -19,7 +19,6 @@ window.onload = function() {
     var authCode = urlWithAuthCode.searchParams.get('code');
     if (authCode) {
         localStorage.setItem('authCode', authCode);
-        localStorage.setItem('redirectUri', 'http://localhost:8000');
         window.location = 'access-token.html';
     }
 }
